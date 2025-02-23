8a6f7de329bb588408508b83098810f744750452
Gasper Martic <martic.gasper@gmail.com>
2025-02-23 20:56:58 +0100

    Implement CLI parser and read bashrc file content
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+use home::home_dir;
+
+const BASHRC_FILENAME: &str = ".bashrc";
+
+#[derive(Parser)]
+struct Args {
+    // name of alias
+    name: String,
+    // system command to be executed by alias
+    command: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("name={}, pattern={}", args.name, args.command);
+
+    let mut bashrc_path = match home_dir() {
+        Some(path) => path,
+        _ => {
+            println!("Unable to get your home dir!");
+            return;
+        },
+    };
+    bashrc_path.push(BASHRC_FILENAME);
+    println!("{:?}", bashrc_path);
+    let content = std::fs::read_to_string(bashrc_path).expect("Could not read bashrc file");
+    println!("{content}");
 }
