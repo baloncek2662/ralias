ff4f5e71f2bc989e4899f571f302a158ec4c3806
Gasper Martic <martic.gasper@gmail.com>
2025-03-22 10:06:36 +0100

    Show also functions
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::io::prelude::*;
 
 use std::{fs::OpenOptions, io::Write, path::PathBuf};
+use std::collections::HashMap;
 
 use clap::{Parser, Subcommand};
 use log::{debug, warn};
@@ -50,7 +51,8 @@ pub enum Operation {
 pub fn run(bashrc_path: &PathBuf, args: Args) -> Result<(), Box<dyn Error>> {
     match args.operation {
         Operation::Show { name } => {
-            show_alias(bashrc_path, name)?;
+            let _ = show_aliases(bashrc_path, &name);
+            let _ = show_funcs(bashrc_path, &name);
         }
         Operation::Add { name, command } => {
             add_alias(bashrc_path, name, command)?;
@@ -65,26 +67,37 @@ pub fn run(bashrc_path: &PathBuf, args: Args) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn show_alias(path: &PathBuf, name: Option<String>) -> Result<(), Box<dyn Error>> {
+// Highlights in red only the part before the separator that matches the search pattern
+fn highlight_search_pattern(line: &str, search_pattern: &str, separator: char) -> String {
+    let parts: Vec<&str> = line.splitn(2, separator).collect();
+    if parts.len() == 2 {
+        let colored = parts[0].replace(search_pattern, &search_pattern.red().bold().to_string());
+        let rest = parts[1];
+        format!("{}{}{}", colored, separator, rest)
+    } else {
+        line.to_string()
+    }
+}
+
+fn show_aliases(path: &PathBuf, name: &Option<String>) -> Result<(), Box<dyn Error>> {
     let contents = std::fs::read_to_string(path).unwrap();
     // Regex to find all lines starting with 'alias', followed by any characters and an equal sign
     let aliases = search(&format!(r"^\s*{ALIAS_PREFIX}.*=.*\s*"), &contents);
     match name {
         Some(name) => {
-            debug!("Show alias: {name}");
+            debug!("Show alias containing: {name}");
             let mut found = false;
             for alias in aliases {
                 // only match left-hand side of the alias - which is the alias name
                 let alias_name = alias.split('=').collect::<Vec<&str>>()[0];
 
-                if alias_name.contains(&name) {
-                    let colored_alias = alias.replace(&name, &name.red().bold().to_string());
-                    println!("{}", colored_alias);
+                if alias_name.contains(name) {
+                    let alias = highlight_search_pattern(alias, name, '=');
+                    println!("{}", alias);
                     found = true;
                 }
             }
             if !found {
-                println!("Alias not found");
                 Err("Alias not found")?
             }
         }
@@ -92,6 +105,54 @@ fn show_alias(path: &PathBuf, name: Option<String>) -> Result<(), Box<dyn Error>
             debug!("Show all aliases");
             for alias in aliases {
                 println!("{}", alias);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn search_bash_functions(contents: &str) -> HashMap<String, String> {
+    let mut result = HashMap::new();
+    let mut curr_func_name = String::new();
+    let mut curr_func: Vec<String> = Vec::new();
+    let mut in_func = false;
+    let query = Regex::new(r"^\s*[a-zA-Z_][a-zA-Z0-9_]*\s*\(\s*\)\s*\{").unwrap();
+    for line in contents.lines() {
+        if query.is_match(line) {
+            // Found a function, what follows is the function body until the closing brace
+            in_func = true;
+            // Get just the function name which will be used as the key in the result hashmap
+            curr_func_name = line.split('(').collect::<Vec<&str>>()[0].trim().to_string();
+            curr_func.push(line.to_string());
+        } else if in_func {
+            curr_func.push(line.to_string());
+            if line.contains("}") {
+                in_func = false;
+                result.insert(curr_func_name.clone(), curr_func.join("\n"));
+                curr_func.clear();
+            }
+        }
+    }
+    result
+}
+
+fn show_funcs(path: &PathBuf, name: &Option<String>) -> Result<(), Box<dyn Error>> {
+    let contents = std::fs::read_to_string(path).unwrap();
+    let funcs = search_bash_functions(contents.as_str());
+    match name {
+        Some(name) => {
+            debug!("Show function containing: {name}");
+            for (func_name, func) in funcs {
+                if func_name.contains(name) {
+                    let func = highlight_search_pattern(&func, name, '{');
+                    println!("{}", func);
+                }
+            }
+        }
+        None => {
+            debug!("Show all functions");
+            for (_, func_body) in funcs {
+                println!("{}", func_body);
             }
         }
     }
@@ -281,16 +342,16 @@ Pick three.";
     #[test]
     fn test_show_alias() {
         let temp_file = helper_create_temp_bashrc();
-        assert!(show_alias(&temp_file.path().to_path_buf(), None).is_ok());
-        assert!(show_alias(&temp_file.path().to_path_buf(), Some(String::from("g"))).is_ok());
-        assert!(show_alias(&temp_file.path().to_path_buf(), Some(String::from("non_existent"))).is_err());
+        assert!(show_aliases(&temp_file.path().to_path_buf(), &None).is_ok());
+        assert!(show_aliases(&temp_file.path().to_path_buf(), &Some(String::from("g"))).is_ok());
+        assert!(show_aliases(&temp_file.path().to_path_buf(), &Some(String::from("non_existent"))).is_err());
     }
 
     #[test]
     fn test_remove_alias() {
         let temp_file = helper_create_temp_bashrc();
-        assert!(show_alias(&temp_file.path().to_path_buf(), Some(String::from("g"))).is_ok());
+        assert!(show_aliases(&temp_file.path().to_path_buf(), &Some(String::from("g"))).is_ok());
         assert!(remove_alias(&temp_file.path().to_path_buf(), String::from("g")).is_ok());
-        assert!(show_alias(&temp_file.path().to_path_buf(), Some(String::from("g"))).is_err());
+        assert!(show_aliases(&temp_file.path().to_path_buf(), &Some(String::from("g"))).is_err());
     }
 }
